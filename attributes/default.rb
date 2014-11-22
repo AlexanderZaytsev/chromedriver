c0cf4feb5c11dcbd77cce99109a0e8730e3e7f37
Alexander Zaytsev <alexander@say26.com>
2014-11-22 21:01:07 +0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
-default[:chromedriver][:path] = '/usr/bin'
+default[:chromedriver][:path] = '/usr/local/bin'
 default[:chromedriver][:owner] = 'root'
 default[:chromedriver][:group] = 'root'
 default[:chromedriver][:mode] = '755'
 default[:chromedriver][:version] = '2.12'
 default[:chromedriver][:archive_name]  = "chromedriver_linux64.zip"
 default[:chromedriver][:source_url]  = "http://chromedriver.storage.googleapis.com/#{node[:chromedriver][:version]}/#{node[:chromedriver][:archive_name]}"
-default[:chromedriver][:full_path] = "#{node[:chromedriver][:path]}/#{node[:chromedriver][:archive_name]}"
+default[:chromedriver][:full_path] = "/tmp/#{node[:chromedriver][:archive_name]}"
